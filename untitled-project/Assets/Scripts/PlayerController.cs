d28f404f89b0fb77555d44d332f07e27844519e8
JD2895 <j.davis.2895@gmail.com>
2019-10-01 00:47:24 -0400

    added basic player controlelr

---
+++ untitled-project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    public float moveSpeed;
+
+    private float vInput;
+    private float hInput;
+
+    void Start()
+    {
+        _rb = this.GetComponent<Rigidbody>();    
+    }
+
+    void FixedUpdate()
+    {
+        vInput = Input.GetAxis("Vertical");
+        hInput = Input.GetAxis("Horizontal");
+        Vector3 vdisplacement = this.transform.forward * vInput * Time.fixedDeltaTime;
+        Vector3 hdisplacement = this.transform.right * hInput * Time.fixedDeltaTime;
+        _rb.MovePosition(this.transform.position + (vdisplacement + hdisplacement) * moveSpeed * Time.fixedDeltaTime);
+    }
+}
