a4477baf1e4a6fcdd798ff1eefc5743f73f6efb6
JD2895 <j.davis.2895@gmail.com>
2019-10-14 00:37:56 -0400

    Added basic camera follow, and a couple of random objects to move around

---
+++ untitled-project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform toFollow;
+    public float xOffset;
+    public float yOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newCameraPosition = toFollow.position;
+        newCameraPosition = newCameraPosition - (Vector3.forward * xOffset);
+        newCameraPosition = newCameraPosition + (Vector3.up * yOffset);
+        transform.position = newCameraPosition;
+        transform.LookAt(toFollow);
+    }
+}
